1694bb1e1dd57ee53763e79a8c61997f474e3d98
TITANS911 <112262438+TITANS911@users.noreply.github.com>
2026-06-06 03:21:20 +0700

    Fix vite config to support JS in .js files

--- terralog-frontend/vite.config.js
+++ terralog-frontend/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    include: ['**/*.jsx', '**/*.js']  // Izinkan file .js berisi JSX
+  })],
   build: {
     outDir: 'build'
   },
