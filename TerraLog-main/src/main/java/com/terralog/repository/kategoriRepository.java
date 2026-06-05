9772173042deb24a4653103b3772e599bbc15db8
TITANS911 <112262438+TITANS911@users.noreply.github.com>
2026-06-05 23:10:11 +0700

    Add H2 database, fix Railway deployment config

--- TerraLog-main/src/main/java/com/terralog/repository/kategoriRepository.java
+++
@@ -1,9 +0,0 @@
-package com.terralog.repository;
-
-import com.terralog.model.kategoriModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface kategoriRepository extends JpaRepository<kategoriModel, Integer> {
-}
